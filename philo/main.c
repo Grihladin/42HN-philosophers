ade966fc5d32eb1e77b78b65f2c701d020c99835
mratke <mratke@student.42heilbronn.de>
2024-12-17 22:31:56 +0100

    now i know how i will calculate time made a finction

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 19:39:16 by mratke            #+#    #+#             */
-/*   Updated: 2024/12/17 21:11:15 by mratke           ###   ########.fr       */
+/*   Updated: 2024/12/17 22:28:58 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,23 @@ void	print_input_vars(t_input_vars *input)
 	printf("var4: %d\n", input->time_to_sleep);
 }
 
+long	time_spended(t_timeval *start)
+{
+	t_timeval	current;
+	long		start_microsec;
+	long		current_microsec;
+
+	gettimeofday(&current, NULL);
+	start_microsec = (start->tv_sec * 1000000) + start->tv_usec;
+	current_microsec = (current.tv_sec * 1000000) + current.tv_usec;
+	return (current_microsec - start_microsec);
+}
+
 int	main(int argc, char **argv)
 {
 	t_input_vars	*input;
 	pthread_t		philo_id;
-	t_timeval		tv;
+	t_timeval		start;
 
 	if (argc != 5)
 	{
@@ -34,9 +46,8 @@ int	main(int argc, char **argv)
 	philo_id = NULL;
 	input = malloc(sizeof(t_input_vars));
 	get_input(input, argv);
-	gettimeofday(&tv, NULL);
-	printf("Current time: %ld seconds and %d microseconds\n", tv.tv_sec,
-		tv.nsec);
+	gettimeofday(&start, NULL);
 	pthread_create(&philo_id, NULL, (void *)print_input_vars, input);
 	pthread_join(philo_id, NULL);
+	printf("Elapsed time: %ld microseconds\n", time_spended(&start));
 }
